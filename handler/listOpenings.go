f0fa7a78877c01ec7389f7f7e958f6ca95f909f2
costa38r <costa38r@proton.me>
2024-12-14 21:39:46 -0300

    listing

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/costa38r/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpenings(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(c, "listing", openings)
 }
